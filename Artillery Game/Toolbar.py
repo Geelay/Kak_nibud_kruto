fb71bab90388d3b2ac9ad4c16815f36264b37443
KTune <43672840+KTune@users.noreply.github.com>
2018-12-14 02:25:43 +0300

    added commands

--- Artillery Game/Toolbar.py
+++ Artillery Game/Toolbar.py
@@ -21,21 +21,38 @@ class Shop(Frame):
         toolbar = Frame(self.parent, bd=3, relief=RAISED)
 
         shieldbutton = Button(toolbar, text="ЗАЩИТА", relief=FLAT, width=10, height=6, font='arial 12', bg="#FF5733",
-                              fg="#641E16", activebackground="#F4D03F")
+                              fg="#641E16", highlightcolor="#CB4335", activebackground="#F4D03F")
         firebutton = Button(toolbar, text="ОРУЖИЕ", relief=FLAT, width=10, height=6, font='arial 12', bg="#FF5733",
-                            fg="#641E16", activebackground="#F4D03F")
-        powerupbutton = Button(toolbar, text="УЛУЧШЕНИЕ", relief=FLAT, width=10, height=6, font='arial 12',
-                               bg="#FF5733",  fg="#641E16",  activebackground="#F4D03F")
+                            fg="#641E16",  highlightcolor="#CB4335", activebackground="#F4D03F")
+        powerbutton = Button(toolbar, text="УЛУЧШЕНИЕ", relief=FLAT, width=10, height=6, font='arial 12',
+                             bg="#FF5733",  fg="#641E16", highlightcolor="#CB4335", activebackground="#F4D03F"),
         otherbutton = Button(toolbar, text="ПРОЧЕЕ", relief=FLAT, width=10, height=6, font='arial 12', bg="#FF5733",
-                             fg="#641E16",  activebackground="#F4D03F")
+                             fg="#641E16",  highlightcolor="#CB4335", activebackground="#F4D03F")
         shieldbutton.pack(side=TOP, padx=2, pady=2)
         firebutton.pack(side=TOP, padx=2, pady=2)
-        powerupbutton.pack(side=TOP, padx=2, pady=2)
+        powerbutton.pack(side=TOP, padx=2, pady=2)
         otherbutton.pack(side=TOP, padx=2, pady=2)
 
         toolbar.pack(side=RIGHT, fill=X)
         self.parent.config(menu=menubar)
         self.pack()
+
+        def shieldbutton_click(event):
+            print ("установить защиту")
+
+        def firebutton_click(event):
+            print ("выбрать оружие")
+
+        def powerbutton_click(event):
+            print ("применить улучшение")
+
+        def otherbutton_click(event):
+            print ("открыть другие возможности")
+
+        shieldbutton.bind("<Button-1>", shieldbutton_click)
+        firebutton.bind("<Button-1>", firebutton_click)
+        powerbutton.bind("<Button-1>", powerbutton_click)
+        otherbutton.bind("<Button-1>", otherbutton_click)
 
     def onExit(self):
         self.quit()
